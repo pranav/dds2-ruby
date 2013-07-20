c4387f1f1681763d035ff73a446ea53060f57bf5
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-07-20 20:48:50 +0000

    Got the uidNumber for logged in accounts

--- ldap.rb
+++ ldap.rb
@@ -7,16 +7,19 @@ require 'config'
 # else return False
 def login(user, pass)
   conn = LDAP::Conn.new($ldap_conf[:server], $ldap_conf[:port])
-  conn.set_option( LDAP::LDAP_OPT_PROTOCOL_VERSION, 3 )
+  conn.set_option(LDAP::LDAP_OPT_PROTOCOL_VERSION, 3)
   conn.start_tls
-  puts user + $ldap_conf[:people_dn]
-  conn.bind('uid=' + user + ',' + $ldap_conf[:people_dn], pass)
+
+  if conn.bind('uid=' + user + ',' + $ldap_conf[:people_dn], pass)
+    get_uidNumber conn, user
+  else
+    False
+  end
 end
 
 
-
-def get_uid(ldap, user)
-  ldap.search(:base => $ldap_conf[:people_dn], :filter => "(cn=#{user})") do |entry|
+def get_uidNumber(conn, user)
+  conn.search($ldap_conf[:people_dn], 1 ,"(uid=#{user})") do |entry|
     entry['uidNumber']
   end
 end
