e92f1b0abd328e18b6280695704f65a9217dfb10
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-09-22 19:32:29 +0000

    Added a DeckMigration class and updated the uploader

--- class/deck.rb
+++ class/deck.rb
@@ -6,3 +6,17 @@ class Deck < ActiveRecord::Base
   has_and_belongs_to_many :machine
 
 end
+
+class DeckMigration < ActiveRecord::Migration
+  def up
+    create_table :deck do |t|
+      t.string :name
+      t.string :uuid
+      t.integer :uid
+    end
+  end
+
+  def down
+    drop_table :deck
+  end
+end
--- controller.rb
+++ controller.rb
@@ -5,13 +5,15 @@ require 'sinatra/cookies'
 require 'erb'
 require 'yaml'
 require 'ldap'
+require 'uuid'
 require 'active_record'
 require 'config.rb'
 require 'login.rb'
+require 'class/deck.rb'
 
 set :bind, '0.0.0.0'
 
-
+# Handle the / url
 get '/' do
   puts "Found a cookie! #{cookies[:auth]}"
   if cookies[:auth]
@@ -22,19 +24,36 @@ get '/' do
 end
 
 
+# GET /upload should just return the pdf upload page.
 get '/upload' do
   erb :upload
 end
 
 
+# POST /upload should check if the file is a PDF and put it in the queue
+# After the queue, it should get converted and put in /slides/ and the db
 post '/upload' do
-  d = Deck.new
-  d.name = params[:name]
-  d.uid = cookies[:auth].to_s()
-  if params[:file]
-    filename = params[:file][:filename]
-    file = params[:file][:tempfile]
-    File.open("slides/#{uuid}
+  if not cookies[:auth]
+    redirect to '/login'
+  end
+  if params[:file] and params[:name]
+    uuid = UUID.new.generate
+
+    # Write the file to the queue folder
+    File.open('queue/' + uuid + '.pdf', 'w') do |f|
+      f.write(params[:file][:tempfile].read)
+    end
+
+    # Convert the pdf into JPEGs
+    `mkdir ./slides/#{uuid}/;`
+    `convert -density 400 ./queue/#{uuid}.pdf -scale 1920x1080 ./slides/#{uuid}/slide.jpg;`
+
+    d = Deck.new
+    d.name = params[:name]
+    d.uuid = uuid
+    d.uid = cookies[:auth]
+    d.save!
+  end
 end
 
 
